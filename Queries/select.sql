3871ac4a564f59d117daf2010dd5c456213a703f
furkangergin <hunter_1325@hotmail.com>
2022-10-24 09:40:41 +0300

    commit2

--- Queries/select.sql
+++ Queries/select.sql
@@ -7,7 +7,7 @@ select * from departments;
 select first_name from employees;
 --get only firstname from employees table
 
---displaty city names
+--display city names
 select city from locations;
 
 --get me first_name,last_name and salary
@@ -16,5 +16,6 @@ select first_name,last_name,salary from employees;
 --get me street address and postal code
 SELECT street_address, postal_code
 FROM locations;
+
 select distinct first_name from employees;
 --remove duplicates
